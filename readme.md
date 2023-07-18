9b973aa8e376145d95a3c1646e0879eb979180e0
Derek Malone <77118878+DerekMalone@users.noreply.github.com>
2023-07-18 10:21:23 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Macho Moods
+
+<aside>
+<strong>In this project you will:</strong> Build a wireframe, write CSS with Flexbox, generate HTML with Javascript, and write modular code
+</aside>
+
+## "There are no tough guys in wrestling!"
+
+<img src="./assets/MachoManSavage.jpeg" width="500px" align="right" />
+People deal with a variety of emotions every day, including wrestling fans. Macho Man, Randy Savage, realizes this and has taken an interest in mental health. He believes he can encourage and uplift his fan base through a multitude of emotions and feelings with his famous quotes. He’s hired you - a team of macho developers - to build this self help site to encourage those going through big emotions. 
+
+He has some very specific design requirements in mind, which surprised you because you didn’t expect a professional wrestler to know or care anything about UI/UX design. 
+
+### This is what Mr. Savage is asking you to build: 
+
+> At the top of the page should be the name of the motivational site, “Macho Moods”, centered on the page. Beneath the title should be a prompt, “How are you feeling today?”
+>
+>Underneath this, all nine moods and encouraging quotes should be displayed in a three-column format. Each mood should have the following format: 
+>
+>The name of the mood should be at the top and centered. The image for the mood should be beneath the name. Under the image should be a box containing the encouraging quotes for the mood. The box should have “Macho Advice:” on the left and the two quotes vertically aligned on the right.
+>
+>Mr. Savage is a fan of flare. He expects loud fonts, bright colors, shadows, rounded borders, and more. He wants you to have creative freedom, but if you present him with a boring, black and white, jumbled mess, he’ll probably smash it to pieces.
+> 
+
+Before you even think about coding, discuss and plan this project with your team. The first step of planning is to build a wireframe, this way you all have a clear goal in mind of what you’re building. Don’t worry about what colors or fonts you will use in the wireframe. Just create a general mockup of what Randy has described.
---
+++ scripts/database.js
@@ -0,0 +1,92 @@
+const database = [
+    {
+        id: 1,
+        name: 'Uncomfortable',
+        imageUrl: 'assets/uncomfortable.jpeg',
+        text: "Unjustifiably in a position I'd rather not be in",
+        quotes: [
+            "Follow the decisions and thoughts of your heart.", 
+            "Sometimes all an annoying person needs is a punch in the face."
+        ]
+    },
+    {
+        id: 2,
+        name: 'Anxious',
+        imageUrl: 'assets/anxious.jpeg',
+        text: "I'm always jittery, it's my personality",
+        quotes: [
+            "Life gets easier when you don’t take things too seriously.", 
+            "Who needs therapy when you can go to the gym."
+        ]
+    },   
+    {
+        id: 3,
+        name: 'Astonished',
+        imageUrl: 'assets/astonished.jpeg',
+        text: "Happened so fast you can't even talk about it",
+        quotes: [
+            "Expect the unexpected in the kingdom of madness!", 
+            "It is the simple truth of life that you have to cope with every situation or chaos thrown at you."
+        ]
+    },
+    {
+        id: 4,
+        name: 'Focused',
+        imageUrl: 'assets/focused.jpeg',
+        text: "I am erasing your face right now. Your face doesn't exist",
+        quotes: [
+            "If you put your heart and mind into achieving your goal, you will surely achieve it one day.", 
+            "A macho man works hard for his dreams."
+        ]
+    },
+    {
+        id: 5,
+        name: 'Hungry',
+        imageUrl: 'assets/hungry.jpeg',
+        text: "Snap into a slim jim!",
+        quotes: [
+            "Cheat meals sound like a treat when you have been working hard all week.",
+            "I always like to keep my favorite snack handy. Need a little excitement? Snap into a Slim Jim!"
+        ]
+    },
+    {
+        id: 6,
+        name: 'Confident',
+        imageUrl: 'assets/confident.jpeg',
+        text: "The cream rises to the top. I am the cream",
+        quotes: [
+            "Love yourself but never stop working on yourself so that you can improve day by day.", 
+            "Don’t think that you are tougher than me, I have been in the danger zone many times."
+        ]
+    }, 
+    {
+        id: 7,
+        name: 'Apathetic',
+        imageUrl: 'assets/apathetic.jpeg',
+        text: "Nothing means nothing",
+        quotes: [
+            "If you want to achieve more in your life, aim for greater things in life.", 
+            "Even a macho man can’t avoid being emotionally attached to people."
+        ]
+    },
+    {
+        id: 8,
+        name: 'Playful',
+        imageUrl: 'assets/playful.jpeg',
+        text: "I've got you for three minutes! Three minutes of playtime.",
+        quotes: [
+            "All I care about is that people are being entertained. It’s not about being the world champion, it’s about exciting the people. If you walk away entertained, then I did my job, and that’s all I care about.", 
+            "Let me tell you now a man of my position can afford to look ridiculous at any time."
+        ]
+    },
+    {
+        id: 9,
+        name: 'Unappreciated',
+        imageUrl: 'assets/unappreciated.jpeg',
+        text: "You are nothing but garbage",
+        quotes: [
+            "Don’t worry if there is no one to pat your back, and you can always give yourself a pat on the back to appreciate yourself.", 
+            "Humble down your haters with your muscles."
+        ]
+    }
+];
